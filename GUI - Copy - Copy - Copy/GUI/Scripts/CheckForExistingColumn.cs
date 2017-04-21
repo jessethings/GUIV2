56a6114b95b7376f307735f6c4044cea72618ecf
bhcode <brittney.huxford@gmail.com>
2017-04-22 10:40:28 +1200

    CheckForExistingColumns added
    Added MySql Nuget package and a class for checking existing columns on
    the server DB. Have not entered connection details yet or tested.

---
+++ GUI - Copy - Copy - Copy/GUI/Scripts/CheckForExistingColumn.cs
@@ -0,0 +1,101 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI.Scripts
+{
+    public static class CheckForExistingColumn
+    {
+        //db variables
+        static string _server;
+        static string _db;
+        static string _uid;
+        static string _pw;
+        static MySqlConnection connection;
+
+
+        private static void establishConnection()
+        {
+            _server = "";
+            _db = "";
+            _uid = "";
+            _pw = "";
+            string constring = $"SERVER={_server};DATABASE={_db};UID={_uid};PASSWORD={_pw};";
+            connection = new MySqlConnection(constring);
+        }
+
+        private static bool openConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch(MySqlException e)
+            {
+                Debug.WriteLine(e.ToString());
+                return false;
+            }
+        }
+
+        private static bool closeConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException e)
+            {
+                Debug.WriteLine(e.ToString());
+                return false;
+            }
+        }
+
+        public static  bool checkForExistingColumnDate(string table, string date, int farmID)
+        {
+            string sql = $"select date FROM {table} where sdate = '{date}' AND farmid = '{farmID}'";
+            establishConnection();
+            if (openConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(sql, connection);
+                //look in table for existence of a column that matches the date and farm id
+                if (cmd.ExecuteScalar() != null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            //cannot confirm or deny contents of table so return true
+            return true;
+        }
+
+        public static bool checkForExistingLabelOrCalc(string table, string rowid)
+        {
+            string sql = $"select date FROM {table} where row = '{rowid}'";
+            establishConnection();
+            if (openConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(sql, connection);
+                //look in table for existence of a column that matches the row
+                if (cmd.ExecuteScalar() != null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            //cannot confirm or deny contents of table so return true
+            return true;
+        }
+    }
+}
